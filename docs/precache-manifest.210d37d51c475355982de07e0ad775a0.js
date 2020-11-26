f435f4b84ef60ade5cdb4c3930d9a45177db3f4e
Byevenes <zneel.yevenes@gmail.com>
2020-11-26 19:36:34 -0300

    docs cambiado nombre

---
+++ docs/precache-manifest.210d37d51c475355982de07e0ad775a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1189418006d9e7466db8e23178b048ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ad16ccfc3cb3e3af627",
+    "url": "/static/js/2.70cfb9b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.70cfb9b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7aa6a83973733e68196",
+    "url": "/static/js/main.8549a29a.chunk.js"
+  },
+  {
+    "revision": "ba6577e94ef88e217a9f",
+    "url": "/static/js/runtime-main.a847c7e3.js"
+  }
+]);
